60ef452604379ee742ee012e8554cadcd59f6b48
Soham Shinde <sohamsshinde04@gmail.com>
2024-05-05 17:15:34 +0400

    changes

--- Assets/Scripts/Patrol.cs
+++ Assets/Scripts/Patrol.cs
@@ -8,32 +8,6 @@ public class Patrol : State
     public Chase chase;
     public bool isPlayerDetected;
 
-    [SerializeField] private float patrolSpeed;
-
-    [SerializeField] public int pointIndex;
-
-    [SerializeField] private Transform[] points;
-
-    [SerializeField] public Rigidbody2D enemyRb;
-
-
-
-    public void EnemyPatrol()
-    {
-        if(!isPlayerDetected)
-        if (Vector2.Distance(transform.position, points[pointIndex].position) < 0.01f)
-        {
-            pointIndex += 1;
-
-            if (pointIndex == points.Length)
-            {
-                pointIndex = 0;
-            }
-        }
-
-        transform.position = Vector2.Lerp(transform.position, points[pointIndex].position, patrolSpeed * Time.deltaTime);
-    }
-
     public override State RunState()
     {
         if (isPlayerDetected)
